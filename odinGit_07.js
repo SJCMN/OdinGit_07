83e40fcb82c3413f03a98986372fcb3f46b28dc3
SJCMN <sam.clavette@gmail.com>
2020-12-19 14:22:35 -0600

    Function working

--- odinGit_07.js
+++ odinGit_07.js
@@ -1,95 +1,73 @@
+
+let playerSelection; // declare player selection
+
+//prompt player to choose(type) 'Rock' || 'Paper' || 'Scissors'
+function getPlayerSelection () {
+playerSelection = prompt('Type: Rock, Paper, or Scissors to play');
+console.log(playerSelection);
+}
+
+let computerSelection;
+
+const gameResult = ['Rock Crushes Scissors', 'Scissors Cuts Paper', 'Paper Covers Rock'];
+const gameMessage = ['You Win!' , 'Try Again!'];
+const gameScore = [];
+
+
 //computer plays randomly returns 'Rock','Paper', or 'Scissors'
 
-const compArray = ["Rock", "Paper", "Scissors"];
+const compArray = ['Rock', 'Paper', 'Scissors'];
 
 function computerPlay() {
-    let compChoice = Math.floor(Math.random() * compArray.length);
-    return compArray[compChoice];
+    let computerChoice = Math.floor(Math.random() * compArray.length);
+    computerSelection = compArray[computerChoice];
   }
-
 
 //round one play takes two paramaters(player selection, computer selection)
 //return 'Player One You Win!' || 'Player One You Loose' + 'rock beats scissors' || 'paper beats rock' || 'scissors beats paper'
-//prompt player to choose(type) 'Rock' || 'Paper' || 'Scissors'
 
-const playerSelection = prompt('Type rock, paper, or scissors to play');
-const computerSelection = computerPlay();
+function roundOne() {
+  computerPlay();
+  getPlayerSelection();
+  if (playerSelection === computerSelection) {
+    return ('Tie Game');
+  }
+  else if (
+          (playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+          (playerSelection === 'Paper' && computerSelection === 'Rock') || 
+          (playerSelection === 'Scissors' && computerSelection === 'Paper')
+          ) {
+  return ('You Win!'); 
+  }
+  else {
+    return ('Try Again!'); 
+    }
+  }
 
-function game() {
-    roundOne(playerSelection, computerSelection);
-    return(roundOne(playerSelection, computerSelection));
+      
+
+//eval code 
+/*
+
+function generateMessage(winningChoice) {
+  let message = "";
+  switch (winningChoice) {
+    case "rock":
+      message = "Rock smashes scissors.";
+      break;
+    case "paper":
+      message = "Paper covers rock.";
+      break;
+    case "scissors":
+      message = "Scissors cuts papper.";
+  }
+  return message;
 }
+
+//eval code
+*/
 
 //plays best of 3 or best of 5 rounds 
 //Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
   
-  function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay().toLowerCase();
-    playerSelection = playerSelection.toLowerCase();
-    if (computerSelection == playerSelection) {
-      displayResults("Tie game!");
-    } else if (
-      (computerSelection == "rock" && playerSelection == "scissors") ||
-      (computerSelection == "scissors" && playerSelection == "paper") ||
-      (computerSelection == "paper" && playerSelection == "rock")
-    ) {
-      computerScore = ++computerScore;
-      keepCpuScore();
-      if (computerScore === 1) {
-        displayResults(
-          `Oh no! You lost.
-          ${capitalize(computerSelection)} beats ${playerSelection}.`
-        );
-      } else if (computerScore === 2) {
-        displayResults(
-          `Arghh. ${capitalize(
-            computerSelection
-          )} beats ${playerSelection}. Give it another shot!`
-        );
-      } else if (computerScore === 3) {
-        displayResults(
-          `${capitalize(
-            computerSelection
-          )} beats ${playerSelection}. It's ok. You got this!!`
-        );
-      } else if (computerScore === 4) {
-        displayResults(
-          `Oh no. It's match point!! ${capitalize(
-            computerSelection
-          )} beats ${playerSelection}. Don't let us down!`
-        );
-      } else {
-        displayResults(`${computerSelection} beats ${playerSelection}`);
-      }
-    } else {
-      playerScore = ++playerScore;
-      keepPlayerScore();
-      if (playerScore === 1) {
-        displayResults(
-          `Lets go!!! You won.
-          ${capitalize(playerSelection)} beats ${computerSelection}.`
-        );
-      } else if (playerScore === 2) {
-        displayResults(
-          `You're pretty good at this. ${capitalize(
-            playerSelection
-          )} beats ${computerSelection}.`
-        );
-      } else if (playerScore === 3) {
-        displayResults(
-          `${capitalize(
-            playerSelection
-          )} beats ${computerSelection}! Has mankind found its savior??`
-        );
-      } else if (playerScore === 4) {
-        displayResults(
-          `${capitalize(
-            playerSelection
-          )} beats ${computerSelection}. One more and you're a hero!`
-        );
-      } else {
-        displayResults(`${playerSelection} beats ${computerSelection}`);
-      }
-    }
-  }
-  
+//  function playRound(playerSelection, computerSelection);
